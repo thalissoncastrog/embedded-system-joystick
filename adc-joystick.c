ff10edf079c963457ca391133b7dc650fb978d1b
Thalisson Castro <adaothalisson@hotmail.com>
2025-02-17 22:39:01 -0300

    feat: Initialize RGB LED pins in adc-joystick.c

--- adc-joystick.c
+++ adc-joystick.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-
+#define LED_R_PIN 13
+#define LED_G_PIN 11
+#define LED_B_PIN 12
 
 int main()
 {
     stdio_init_all();
+
+    // Initialize the red LED pin
+    gpio_init(LED_R_PIN);
+    gpio_set_dir(LED_R_PIN, GPIO_OUT);
+
+    // Initialize the green LED pin
+    gpio_init(LED_G_PIN);
+    gpio_set_dir(LED_G_PIN, GPIO_OUT);
+
+    // Initialize the blue LED pin
+    gpio_init(LED_B_PIN);
+    gpio_set_dir(LED_B_PIN, GPIO_OUT);
 
     while (true) {
         printf("Hello, world!\n");
